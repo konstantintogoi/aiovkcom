ef3c21f4b8dcb8c421be725daa9ca1e7a559fb30
Konstantin Togoi <konstantin.togoi@gmail.com>
2020-05-08 12:56:31 +0300

    bump version to 0.1.0.post2

--- aiovkcom/__init__.py
+++ aiovkcom/__init__.py
@@ -14,4 +14,4 @@ from .sessions import (
 )
 from .api import API
 
-__version__ = '0.1.0.post1'
+__version__ = '0.1.0.post2'
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Konstantin Togoi'
 author = 'Konstantin Togoi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0.post1'
+release = '0.1.0.post2'
 
 
 # -- General configuration ---------------------------------------------------
